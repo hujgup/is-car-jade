f2ad0dae262a63ccdaaafef406c1642bdc0ce46f
James Leonard <31268700+6734170@users.noreply.github.com>
2017-10-10 13:32:56 +1100

    Changes Made 10/10/17
    changes made to the userinterface folder and the SchldulingAgent in the logic folder

--- src/cos30018/assignment/logic/SchedulingAgent.java
+++ src/cos30018/assignment/logic/SchedulingAgent.java
@@ -1,11 +1,52 @@
 package cos30018.assignment.logic;
 
-import jade.core.Agent;
 
+import jade.core.AID;
+import jade.core.Agent;
+import jade.domain.AMSService;
+import jade.domain.FIPAAgentManagement.AMSAgentDescription;
+import jade.domain.FIPAAgentManagement.SearchConstraints;
+import jade.lang.acl.ACLMessage;
+/*
+@author James
+* The setup function has a method to find the agents that are currently running and gives back the ID
+* This will be used o communicate between them through FIPA ACL Messaging
+* Am Planning on completing this tonight, Please feel free to complete
+
+* TO DO:
+
+	- Create an CarAgent for each Sch Agent
+	- Basic Communication between them and then,
+	- Between Sch Agents
+
+	- I have completed communications in my labs, it is just a matter of implementing them here
+
+	 
+	Issues
+	- How to construct a Car Object using the Car and CarID Java files. 
+	- Not sure how to initialise the LocalTimeRange List
+
+
+*/
 @SuppressWarnings("serial")
 public class SchedulingAgent extends Agent {
 	@Override
 	public void setup() {
+		// Get the current AID running
+		AMSAgentDescription [] agents = null;
+		try {
+			SearchConstraints c = new SearchConstraints();
+			c.setMaxResults(new Long(-1));
+				agents = AMSService.search(this, new AMSAgentDescription(), c);
+		} catch(Exception e) {
+			System.out.println("Problem search AMS: " + e);
+			e.printStackTrace();
+		}
 		
+		AID AgentID = getAID();
+		System.out.println(getAID().getName());
 	}
+	
+	private void update(){};
+	private void broadcast(){};
 }
---
+++ src/cos30018/assignment/userinterface/BasicGui.java
@@ -0,0 +1,102 @@
+package cos30018.assignment.userinterface;
+
+import java.util.Scanner;
+
+import cos30018.assignment.logic.SchedulingAgent;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+import jade.wrapper.StaleProxyException;
+
+/*
+@author James
+
+This is a basic text based gui that naviagates the program
+
+
+*/
+
+
+public class BasicGui {
+	Scanner scan;
+	int selection;
+	
+	
+	
+	public BasicGui()
+	{
+		initMenu();
+		menu();
+	}
+	
+	public void initMenu()
+	{
+		System.out.println("Please specify how many schelduling agents: ");
+		scan = new Scanner(System.in);
+		selection = Integer.parseInt(scan.nextLine());
+		
+		ScheduleAgent(selection);
+	}
+	
+	
+	
+	public void menu()
+	{
+		System.out.println("Please choose a selection: ");
+		System.out.println("1: Register Car");
+		System.out.println("2: TimeTable");
+		System.out.println("3: Exit");
+		System.out.println("Selection: ");
+		scan = new Scanner(System.in);
+		selection = Integer.parseInt(scan.nextLine());
+		
+		switch(selection)
+		{
+			case 1:
+				 System.out.println("Registering your car");
+				 // Check if the car has been registered before
+				 	// If it has then Get the id and return to the menu
+				 	// if not, then make a new Car object
+				 menu();
+				 break;
+			case 2:
+				System.out.println("TimeTable");
+				menu();
+				break;
+			case 3:
+				System.out.println("Go Back");
+				menu();
+				break;
+		}
+	} // end menu
+	
+	// @param num- specifies how many agents you want
+	// Creates the main container with the schduling agents.
+	private void ScheduleAgent(int num){
+		Runtime rt = Runtime.instance();
+		
+		Profile pMain = new ProfileImpl(null,8888,null);
+		pMain.setParameter(Profile.GUI, "true");
+		pMain.setParameter(Profile.CONTAINER_NAME, "Scheduling Agents");
+		ContainerController mainCtrl = rt.createMainContainer(pMain);
+			
+		for(int i = 0; i < num; i++)
+		{
+			try{
+			
+				AgentController schedulingAgents = mainCtrl.createNewAgent("schl"+i, SchedulingAgent.class.getName(), new Object[0]);
+				schedulingAgents.start();
+			
+			}catch(StaleProxyException e){
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
+	
+	
+
+}
