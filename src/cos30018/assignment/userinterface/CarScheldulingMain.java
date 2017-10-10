0fc4380639600385bbe8f108b649c528ad90d297
James Leonard <31268700+6734170@users.noreply.github.com>
2017-10-11 10:45:49 +1100

    Changes Made 10/10/17
    Changes made

---
+++ src/cos30018/assignment/userinterface/CarScheldulingMain.java
@@ -0,0 +1,18 @@
+package cos30018.assignment.userinterface;
+
+
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+import jade.wrapper.StaleProxyException;
+import cos30018.assignment.userinterface.BasicGui;
+
+public class CarScheldulingMain {
+	
+	public static void main(String[] args) {	
+		BasicGui guiInterface = new BasicGui();
+	}
+		
+}// end Car Schelduling Main
