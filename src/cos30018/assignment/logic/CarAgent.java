517394487526ff7434ae3e55088723da17361c77
James Leonard <31268700+6734170@users.noreply.github.com>
2017-10-22 21:54:30 +1100

    Communication
    Changes done to communication. We can now listen for requests within a certain time. The listen class prints out a int array that you can use Will.

--- src/cos30018/assignment/logic/CarAgent.java
+++ src/cos30018/assignment/logic/CarAgent.java
@@ -22,63 +22,85 @@ import cos30018.assignment.utils.SendingMessage;
 
 @SuppressWarnings("serial")
 public class CarAgent extends Agent {
-	private int numOfArguments;
-	private String input;
-	private Scanner scan;
+	//private int numOfArguments;
 	//private SequentialBehaviour listener;
 	
 	 public void setup() {
 		Object[] args = getArguments();
+		String masterAID = (String) args[0];
+		String carAID = (String) args[1];
+		String inputValue = (String) args[2];
 		
-		numOfArguments = args.length;
-		System.out.println(getLocalName()+ ": What time would you like?");
-		scan = new Scanner(System.in);
-		input = scan.nextLine();
+		//numOfArguments = args.length;
 		
-		if(input != null || input != "")
+		if(inputValue != null || inputValue != "")
 		{
-			
-			System.out.println(input);
-			
-			// make it a simple message to and from a agent
-			AID dest = new AID((String) args[0], AID.ISLOCALNAME);
-			
+			AID dest = new AID(masterAID,AID.ISLOCALNAME);
 			ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
-			msg.setContent(input);
+			msg.setContent(inputValue);
 			msg.addReceiver(dest);
 			send(msg);
 			
-			
-			
-//				// Car Object that is intialised at the bottom of this code
-//				//Car car = createCarObject(50.0,100, 20, LocalTime.parse("10:00"), LocalTime.parse("12:00"));
-//				//System.out.println(getLocalName() +": has a charge capacity of "+car.getChargeCapacity());
-//				
-//				System.out.println("Requesting dummy-action to" + numOfArguments + " responders");
-//				
-//				//Create a Request Message
-//				ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
-//				
-//				for(int i=0; i< args.length; i++)
-//				{
-//					// adding the master as a receiver 
-//					msg.addReceiver(new AID((String) args[i], AID.ISLOCALNAME));
-//				}
-//				
-//				// setting the protocol and the content of the message to what the the user inputted
-//				msg.setProtocol(FIPANames.InteractionProtocol.FIPA_REQUEST);
-//				msg.setReplyByDate(new Date(System.currentTimeMillis() + 10000));
-//				// set the message content that the car agent will send change to input
-//				msg.setContent(input);
-//				
-//				addBehaviour(new SendingMessage(this, msg, args));
-		
-		}// end of if statements
-		else
-		{
-			System.out.println("No input mate, deal with it");
+		} else {
+			System.out.println("There was no input");
 		}
 		
-	 } // end of setup 
+		
+} // end of Setup
 	 
-} // end of class
+}// end of class
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		System.out.println(getLocalName()+ ": What time would you like?");
+//		scan = new Scanner(System.in);
+//		input = scan.nextLine();
+//		
+//		if(input != null || input != "")
+//		{
+//			
+//			System.out.println(input);
+//			
+//			// make it a simple message to and from a agent
+//			AID dest = new AID((String) args[0], AID.ISLOCALNAME);
+//			
+//			ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+//			msg.setContent(input);
+//			msg.addReceiver(dest);
+//			send(msg);
+//			
+//			
+//			
+////				// Car Object that is intialised at the bottom of this code
+////				//Car car = createCarObject(50.0,100, 20, LocalTime.parse("10:00"), LocalTime.parse("12:00"));
+////				//System.out.println(getLocalName() +": has a charge capacity of "+car.getChargeCapacity());
+////				
+////				System.out.println("Requesting dummy-action to" + numOfArguments + " responders");
+////				
+////				//Create a Request Message
+////				ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+////				
+////				for(int i=0; i< args.length; i++)
+////				{
+////					// adding the master as a receiver 
+////					msg.addReceiver(new AID((String) args[i], AID.ISLOCALNAME));
+////				}
+////				
+////				// setting the protocol and the content of the message to what the the user inputted
+////				msg.setProtocol(FIPANames.InteractionProtocol.FIPA_REQUEST);
+////				msg.setReplyByDate(new Date(System.currentTimeMillis() + 10000));
+////				// set the message content that the car agent will send change to input
+////				msg.setContent(input);
+////				
+////				addBehaviour(new SendingMessage(this, msg, args));
+		
+		
